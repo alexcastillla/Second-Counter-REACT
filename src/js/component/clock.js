8ac3443839d1ba4f28d2b4376d4007c8fee73447
alexcastillla <alexander.zcas@gmail.com>
2020-10-22 09:19:36 +0000

    Added second counter, v1 - Only count

---
+++ src/js/component/clock.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import ReactDOM from "react-dom";
+import { propTypes } from "react-bootstrap/esm/Image";
+
+export const SecondsCounter = () => {
+	const [timer, setTimer] = useState(0);
+
+	useEffect(() => {
+		window.setInterval(() => {
+			setTimer(time => time + 1);
+		}, 1000);
+	}, []);
+
+	return (
+		<div className="SecondsCounter">
+			<h1>
+				Tiempo que llevas perdido en arreglar este ejercicio: {timer}{" "}
+				segundos
+			</h1>
+		</div>
+	);
+};
